6c1874a4735e297cefb6d4acd32de6648600878f
greysoh <dev@hofers.cloud>
2023-02-27 16:41:19 -0500

    chore: Resets repository, for new approach.

--- engine/mod.js
+++
@@ -1,9 +0,0 @@
-import { tokenize } from "./parsing/mod.js";
-import { convert } from "./conversion/convert.js";
-
-export function compile(text) {
-  const lex = tokenize(text);
-  const json = convert(lex);
-
-  return json;
-}
